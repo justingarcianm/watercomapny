f6bde15579e05a1d923e15ea4f8b1e5476e1e9d6
justin garcia <justinframedev@gmail.com>
2019-02-21 16:24:21 -0700

    add custom styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// set up
 const express = require("express"),
     app = express(),
     port = 3000;
@@ -6,7 +6,13 @@ const express = require("express"),
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+
+// routes
 app.get("/", (req, res) => res.render("landing"))
 
-app.listen(port, () => console.log("Server running at http://localhost:3000/"));
 
+
+
+
+
+app.listen(port, () => console.log("Server running at http://localhost:3000/"));
